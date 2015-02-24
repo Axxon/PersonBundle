55cdc841e87df6823a8d6670bf6f53bd230ac335
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2015-02-24 02:10:36 +0100

    Oups! I Refactoring-it again.

--- BlackPersonBundle.php
+++ BlackPersonBundle.php
@@ -1,25 +1,50 @@
 <?php
 
-/*
- * This file is part of the Black package.
- *
- * (c) Alexandre Balmes <albalmes@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Black\Bundle\PersonBundle;
 
+use Black\Bundle\UserBundle\Application\DependencyInjection\BlackUserExtension;
+use Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Console\Application;
 
-/**
- * Class BlackPersonBundle
- *
- * @package Black\Bundle\PersonBundle
- * @author  Alexandre Balmes <albalmes@gmail.com>
- * @license http://opensource.org/licenses/mit-license.php MIT
- */
-class BlackPersonBundle extends Bundle
+class BlackUserBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new BlackUserExtension();
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $mappings = array(
+            realpath($this->getPath() . '/Resources/config/doctrine/model') => 'Black\Component\Person\Domain\Model',
+        );
+
+        $ormCompilerClass = 'Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass';
+
+        if (class_exists($ormCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineOrmMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    [],
+                    'application.backend_type_orm'
+                ));
+        }
+
+        $mongoCompilerClass = 'Doctrine\Bundle\MongoDBBundle\DependencyInjection\Compiler\DoctrineMongoDBMappingsPass';
+
+        if (class_exists($mongoCompilerClass)) {
+            $container->addCompilerPass(
+                DoctrineMongoDBMappingsPass::createXmlMappingDriver(
+                    $mappings,
+                    [],
+                    'application.backend_type_mongodb'
+                ));
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 PersonBundle
-============
+==========
 
 [WIP] Person Management
